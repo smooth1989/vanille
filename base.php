8bf975d434505a316fbcab1b3d53998bd54f7846
Ivan Filsky <justfdot@gmail.com>
2015-04-02 03:28:45 +0500

    new base markup

---
+++ base.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Vanille</title>
+    <link rel="stylesheet" href="css/base.css" />
+</head>
+<body>
+<div class="header">
+    <div class="container">
+        <a class="logo" href="/"></a>
+        <ul class="contacts">
+            <li class="phone">+7 (351) 795-06-19</li>
+            <li><a href="mailto:example@gmail.com">example@gmail.com</a></li>
+        </ul>
+        <ul class="menu">
+            <li><a class="bordered" href="#">Печенье и выпечка</a></li>
+            <li><a class="bordered" href="#">Где заказать</a></li>
+            <li><a class="bordered" href="#">Новости</a></li>
+            <li><a class="bordered" href="#">О компании</a></li>
+            <li><a class="bordered" href="#">Контакты</a></li>
+            <li><a class="bordered" href="#">Порядок работы</a></li>
+        </ul>
+    </div>
+</div>
+<div class="wrapper">
+    <div class="workarea">
+        <div class="container">
+            <h1>Печенье и выпечка</h1>
+            <ul class="goods-filter">
+                <li><a class="bordered" href="#">Сейчас в производстве</a></li>
+                <li><a class="bordered" href="#">Весь каталог</a></li>
+                <li><a class="bordered" href="#">Самые популярные</a></li>
+            </ul>
+            <div class="side">
+                <div class="current-good">
+                    <img src="http://placehold.it/250x250" alt="alt text" />
+                    <h3>Мишка шалунишка со сгущенкой</h3>
+                    <ul class="details">
+                        <li>Расфасован по <span>1,7 кг</span> в упаковке</li>
+                        <li>Срок хранения <span>1,5</span> месяца</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="main">
+                <ul class="goods">
+                    <li class="clearfix">
+                        <img src="http://placehold.it/150x150" alt="alt text" />
+                        <a class="bordered" href="#">some texty text</a>
+                    </li>
+                    <li class="clearfix">
+                        <img src="http://placehold.it/150x150" alt="alt text" />
+                        <a class="bordered" href="#">some texty texty texty texty texty texty texty texty texty text</a>
+                    </li>
+                    <li class="clearfix">
+                        <img src="http://placehold.it/150x150" alt="alt text" />
+                        <a class="bordered" href="#">some texty text</a>
+                    </li>
+                    <li class="clearfix">
+                        <img src="http://placehold.it/150x150" alt="alt text" />
+                        <a class="bordered" href="#">some texty text</a>
+                    </li>
+                </ul>
+                <ul class="goods">
+                    <li class="clearfix">
+                        <img src="http://placehold.it/150x150" alt="alt text" />
+                        <a class="bordered" href="#">some texty texty texty texty text</a>
+                    </li>
+                    <li class="clearfix">
+                        <img src="http://placehold.it/150x150" alt="alt text" />
+                        <a class="bordered" href="#">some texty text</a>
+                    </li>
+                    <li class="clearfix">
+                        <img src="http://placehold.it/150x150" alt="alt text" />
+                        <a class="bordered" href="#">some texty text</a>
+                    </li>
+                    <li class="clearfix">
+                        <img src="http://placehold.it/150x150" alt="alt text" />
+                        <a class="bordered" href="#">some texty text</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+</div>
+<div class="footer">
+    <div class="container">
+        <ul class="footer-menu">
+            <li><a class="bordered" href="#">Контактная информация</a></li>
+            <li><a class="bordered" href="#">Вакансии</a></li>
+            <li><a class="bordered" href="#">Поставщикам</a></li>
+        </ul>
+    </div>
+</div>
+</body>
+</html>
